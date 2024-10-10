bde3e2fad25b8acc9aaa3cf4ea4e85081f9b6ac8
thanks299 <thanksagbeble@gmail.com>
2024-10-10 19:53:59 +0100

    proofile

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Route for the homepage
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Run the app
+if __name__ == '__main__':
+    app.run(debug=True)
+
---
+++ static/scripts.js
@@ -0,0 +1,20 @@
+document.querySelector('.btn-hire').addEventListener('mouseenter', () => {
+    document.querySelector('.btn-hire').style.backgroundColor = '#fff';
+    document.querySelector('.btn-hire').style.color = '#000';
+});
+
+document.querySelector('.btn-hire').addEventListener('mouseleave', () => {
+    document.querySelector('.btn-hire').style.backgroundColor = '#800080';
+    document.querySelector('.btn-hire').style.color = '#fff';
+});
+
+document.querySelector('.btn-works').addEventListener('mouseenter', () => {
+    document.querySelector('.btn-works').style.backgroundColor = '#fff';
+    document.querySelector('.btn-works').style.color = '#000';
+});
+
+document.querySelector('.btn-works').addEventListener('mouseleave', () => {
+    document.querySelector('.btn-works').style.backgroundColor = '#800080';
+    document.querySelector('.btn-works').style.color = '#fff';
+});
+
